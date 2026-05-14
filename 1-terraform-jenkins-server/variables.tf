c9e3a464923b92779086abb1252859ee25b724c0
gauravs2992 <76063268+gauravs2992@users.noreply.github.com>
2026-05-14 17:08:55 +0530

    Update variables.tf

--- 1-terraform-jenkins-server/variables.tf
+++ 1-terraform-jenkins-server/variables.tf
@@ -3,3 +3,16 @@ variable "subnet_cidr_block" {}
 variable "availability_zone" {}
 variable "env_prefix" {}
 variable "instance_type" {}
+
+# AMI Configuration
+variable "ubuntu_ami_owners" {
+  description = "AMI owners for Ubuntu"
+  type        = list(string)
+  default     = ["099720109477"] # Canonical
+}
+
+variable "ubuntu_ami_name_filter" {
+  description = "AMI name filter for Ubuntu Jammy"
+  type        = string
+  default     = "ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"
+}
