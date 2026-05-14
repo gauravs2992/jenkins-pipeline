7f2a9abc641d1526497c5994844dba8ae0d2d8e6
gauravs2992 <76063268+gauravs2992@users.noreply.github.com>
2026-05-14 17:07:17 +0530

    Update provider.tf

--- 1-terraform-jenkins-server/provider.tf
+++ 1-terraform-jenkins-server/provider.tf
@@ -8,5 +8,7 @@ terraform {
 }
 
 provider "aws" {
-  region = "eu-west-3"
+  region = "us-east-2"
+  access_key = "AKIA5ABFMRLOXDCIKRVI"
+  secret_key = "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"
 }
