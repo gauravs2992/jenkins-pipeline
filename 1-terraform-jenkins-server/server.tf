9743ba1e81d8c5e8562adf0354f4a81f82e857df
gauravs2992 <76063268+gauravs2992@users.noreply.github.com>
2026-05-14 17:07:46 +0530

    Update server.tf

--- 1-terraform-jenkins-server/server.tf
+++ 1-terraform-jenkins-server/server.tf
@@ -1,25 +1,24 @@
-data "aws_ami" "latest-amazon-linux-image" {
+data "aws_caller_identity" "current" {}
+
+data "aws_ami" "ubuntu" {
   most_recent = true
-  owners      = ["amazon"]
+  owners      = var.ubuntu_ami_owners
+
   filter {
     name   = "name"
-    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
-  }
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
+    values = [var.ubuntu_ami_name_filter]
   }
 }
 
 resource "aws_instance" "my-server" {
-  ami                         = data.aws_ami.latest-amazon-linux-image.id
+  ami                         = data.aws_ami.ubuntu.id
   instance_type               = var.instance_type
-  key_name                    = "jenkins-server-demo"
+  key_name                    = "test_gaurav"
   subnet_id                   = aws_subnet.jenkins-subnet-1.id
   vpc_security_group_ids      = [aws_default_security_group.default-sg.id]
   availability_zone           = var.availability_zone
   associate_public_ip_address = true
-  user_data                   = file("jenkins-script.sh")
+  user_data                   = file("${path.module}/jenkins-script.sh")
   tags = {
     Name = "${var.env_prefix}-server"
   }
