68a46356777bdd325aa27dd74a389d7bc9a4e887
gauravs2992 <76063268+gauravs2992@users.noreply.github.com>
2026-05-14 17:06:19 +0530

    Update jenkins-script.sh

--- 1-terraform-jenkins-server/jenkins-script.sh
+++ 1-terraform-jenkins-server/jenkins-script.sh
@@ -1,28 +1,102 @@
 #!/bin/bash
+set -e
 
-# install jenkins
+echo "=== Updating system packages ==="
+apt-get update
+apt-get upgrade -y
 
-sudo yum update
-sudo wget -O /etc/yum.repos.d/jenkins.repo \
-    https://pkg.jenkins.io/redhat-stable/jenkins.repo
-sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
-sudo yum upgrade -y
-sudo amazon-linux-extras install java-openjdk11 -y
-sudo yum install jenkins -y
-sudo systemctl enable jenkins
-sudo systemctl start jenkins
+echo "=== Installing Java 21 and dependencies ==="
+apt-get install -y openjdk-21-jdk curl gnupg ca-certificates
 
-# install git
-sudo yum install git -y
+java -version
+update-java-alternatives -s java-21-openjdk-amd64 || true
 
-# install terraform
+echo "=== Adding Jenkins repository ==="
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2026.key | apt-key add -
+echo "deb https://pkg.jenkins.io/debian-stable binary/" | tee /etc/apt/sources.list.d/jenkins.list > /dev/null
 
-sudo yum install -y yum-utils
-sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
-sudo yum -y install terraform
+echo "=== Installing Jenkins ==="
+apt-get update
+apt-get install -y jenkins
 
-# install kubectl
+echo "=== Configuring Jenkins JAVA_HOME ==="
+echo "JAVA_HOME=/usr/lib/jvm/java-21-openjdk-amd64" >> /etc/default/jenkins
+echo "JENKINS_JAVA_CMD=/usr/bin/java" >> /etc/default/jenkins
 
-sudo curl -LO https://storage.googleapis.com/kubernetes-release/release/v1.23.6/bin/linux/amd64/kubectl
-sudo chmod +x ./kubectl
-sudo mkdir -p $HOME/bin && sudo cp ./kubectl $HOME/bin/kubectl && export PATH=$PATH:$HOME/bin
+echo "=== Creating Jenkins init scripts ==="
+mkdir -p /var/lib/jenkins/init.groovy.d
+
+# Create admin user setup script
+cat > /var/lib/jenkins/init.groovy.d/00-create-admin.groovy << 'GROOVY_EOF'
+import hudson.security.HudsonPrivateSecurityRealm
+import hudson.security.FullControlOnceLoggedInAuthorizationStrategy
+import jenkins.model.Jenkins
+import jenkins.install.InstallState
+
+def instance = Jenkins.get()
+if (instance.getSecurityRealm() instanceof HudsonPrivateSecurityRealm) return
+
+println("Creating Jenkins admin user...")
+def realm = new HudsonPrivateSecurityRealm(false)
+realm.createAccount("admin", "gaurav@123")
+instance.setSecurityRealm(realm)
+instance.setAuthorizationStrategy(new FullControlOnceLoggedInAuthorizationStrategy())
+instance.setInstallState(InstallState.INITIAL_SETUP_COMPLETED)
+instance.save()
+GROOVY_EOF
+
+# Create plugin installation script
+cat > /var/lib/jenkins/init.groovy.d/01-install-plugins.groovy << 'GROOVY_EOF'
+import jenkins.model.Jenkins
+import java.util.logging.Logger
+
+def logger = Logger.getLogger("jenkins.init")
+def instance = Jenkins.get()
+def uc = instance.getUpdateCenter()
+def pluginIds = [
+  "git",
+  "workflow-aggregator",
+  "credentials",
+  "mailer",
+  "matrix-auth",
+  "ssh-slaves",
+  "pipeline-stage-view",
+  "blueocean",
+  "job-dsl",
+  "docker-plugin",
+  "cloudbees-folder"
+]
+
+logger.info("Installing recommended Jenkins plugins: " + pluginIds.toString())
+pluginIds.each { id ->
+  if (!instance.pluginManager.getPlugin(id)) {
+    def plugin = uc.getPlugin(id)
+    if (plugin) {
+      logger.info("Deploying plugin " + id)
+      plugin.deploy().get()
+    } else {
+      logger.info("Plugin " + id + " not found in update center")
+    }
+  }
+}
+instance.save()
+GROOVY_EOF
+
+chown -R jenkins:jenkins /var/lib/jenkins/init.groovy.d
+
+echo "=== Setting Jenkins permissions ==="
+chown -R jenkins:jenkins /var/lib/jenkins
+chown -R jenkins:jenkins /var/log/jenkins
+chown -R jenkins:jenkins /var/cache/jenkins
+
+echo "=== Starting Jenkins ==="
+systemctl daemon-reload
+systemctl enable jenkins
+systemctl start jenkins
+
+sleep 10
+
+echo "=== Jenkins Status ==="
+systemctl status jenkins || true
+
+echo "=== Installation Complete ==="
