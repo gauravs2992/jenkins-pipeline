257dd4e24cfd34745c291baf31abe90c33228a3b
gauravs2992 <76063268+gauravs2992@users.noreply.github.com>
2026-05-16 16:26:52 +0530

    Update backend.tf

--- 2-terraform-eks-deployment/backend.tf
+++ 2-terraform-eks-deployment/backend.tf
@@ -1,7 +1,5 @@
 terraform {
-  backend "s3" {
-    bucket = "jenkins-app-kub-2026-v2"
-    region = "us-east-2"
-    key = "eks/terraform.tfstate"
+  backend "local" {
+    path = "C:/Users/gaurav/Downloads/terraform.tfstate"
   }
 }
